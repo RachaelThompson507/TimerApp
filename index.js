69fcfba67c60f5b91e4eecff7cab08a7e706b279
Rachael Thompson <rachael.elizabeth.thompson@gmail.com>
2020-05-26 06:55:22 -0500

    cleanup and prep for timer count down

--- index.js
+++ index.js
@@ -2,20 +2,30 @@
 
 class Timer {
     constructor(durationInput, startBtn, pauseBtn) {
-        this.durationInput = durationInput; 
-        this.startBtn = startBtn; 
-        this.pauseBtn = pauseBtn; 
+        this.durationInput = durationInput;
+        this.startBtn = startBtn;
+        this.pauseBtn = pauseBtn;
 
         this.startBtn.addEventListener('click', this.start);
+        this.pauseBtn.addEventListener('click', this.pause);
+    }
+    // Use arrow f(x) to ensure value of this. = our class
+    start = () => {
+        this.tick();
+        this.interval = setInterval(this.tick, 1000);
+    };
+
+    pause = () => {
+        clearInterval(this.interval);
     }
 
-    start() {
-        console.log('Timer started');
-    }
+    tick = () => {
+        console.log('tick');
+    };
 }
 
 const durationInput = document.querySelector('#duration');
 const startBtn = document.querySelector('#start');
 const pauseBtn = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startBtn, pauseBtn); 
+const timer = new Timer(durationInput, startBtn, pauseBtn);
