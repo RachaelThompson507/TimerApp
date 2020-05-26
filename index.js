3cc414701f8df33ca8edbb04daa6249b70022573
Rachael Thompson <rachael.elizabeth.thompson@gmail.com>
2020-05-26 06:09:31 -0500

    update for init class

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-// Test app is connected to right file
-console.log("Hello World");
+
+
+class Timer {
+    constructor(durationInput, startBtn, pauseBtn) {
+        this.durationInput = durationInput; 
+        this.startBtn = startBtn; 
+        this.pauseBtn = pauseBtn; 
+
+        this.startBtn.addEventListener('click', this.start);
+    }
+
+    start() {
+        console.log('Timer started');
+    }
+}
+
+const durationInput = document.querySelector('#duration');
+const startBtn = document.querySelector('#start');
+const pauseBtn = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startBtn, pauseBtn); 
